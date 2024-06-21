b7881579258afa897c837b680f2b68dd4641b67f
nfdeveloper <nfdeveloper04@gmail.com>
2024-06-21 16:18:45 -0300

    Implementando LoadBalance

---
+++ mscloudgateway/src/test/java/io/github/nfdeveloper/mc/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nfdeveloper.mc.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
