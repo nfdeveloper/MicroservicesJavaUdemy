6309b9a7d40152b7c30f5e0fc8a3981c4206707f
nfdeveloper <nfdeveloper04@gmail.com>
2024-06-21 11:40:57 -0300

    Criando servidor Eureka e Microservicos de Clientes

---
+++ msclientes/src/test/java/io/github/nfdeveloper/mc/msclientes/MsclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nfdeveloper.mc.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
