6d327ace33182fe7ca633ce67e7d145ccd7fff81
nfdeveloper <nfdeveloper04@gmail.com>
2024-06-21 15:55:00 -0300

    Criando funcionalidade para Criação e Exibição de Cliente

---
+++ msclientes/src/main/java/io/github/nfdeveloper/mc/msclientes/application/representation/ClienteSaveRequest.java
@@ -0,0 +1,40 @@
+package io.github.nfdeveloper.mc.msclientes.application.representation;
+
+import io.github.nfdeveloper.mc.msclientes.domain.Cliente;
+
+public class ClienteSaveRequest {
+	private String cpf;
+	private String nome;
+	private Integer idade;
+	
+	public Cliente toModel() {
+		return new Cliente(cpf, nome, idade);
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	
+
+}
